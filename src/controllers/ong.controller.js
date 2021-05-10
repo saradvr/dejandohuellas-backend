8da9d7b5200686a603b68094c2bec2fd6ed80c4b
saradvr <saradvr1608@gmail.com>
2021-05-10 12:56:10 -0500

    solucion bugs y creacion getPublicOng

--- src/controllers/ong.controller.js
+++ src/controllers/ong.controller.js
@@ -6,7 +6,26 @@ module.exports = {
       const {
         user: { userTypeId },
       } = req;
-      const ong = await Ong.findById(userTypeId).populate('animals');
+      const ong = await Ong.findById(userTypeId).populate('animals').populate({
+        path: 'user',
+        select: 'name',
+      });
+      res.status(200).json({ message: 'Fundación cargada con éxito', ong });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: 'No pudo cargarse la información', error });
+    }
+  },
+  async getPublicOng(req, res) {
+    try {
+      const {
+        params: { ongId },
+      } = req;
+      const ong = await Ong.findById(ongId).populate('animals').populate({
+        path: 'user',
+        select: 'name',
+      });
       res.status(200).json({ message: 'Fundación cargada con éxito', ong });
     } catch (error) {
       res
--- src/routes/ong.js
+++ src/routes/ong.js
@@ -1,7 +1,8 @@
-const { getOng } = require('../controllers/ong.controller');
+const { getOng, getPublicOng } = require('../controllers/ong.controller');
 const { auth } = require('../utils/auth');
 const router = require('express').Router();
 
 router.route('/').get(auth, getOng);
+router.route('/:ongId').get(getPublicOng);
 
 module.exports = router;
