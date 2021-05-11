06dba524b483971c3b0b99700bfc2d0598199103
saradvr <saradvr1608@gmail.com>
2021-05-11 00:37:54 -0500

    eliminar animal

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -2,6 +2,7 @@ const {
   create,
   getAnimal,
   update,
+  deleteAnimal,
 } = require('../controllers/animal.controller');
 const { auth } = require('../utils/auth');
 const { formData } = require('../utils/formData');
@@ -11,5 +12,6 @@ const router = require('express').Router();
 router.route('/new').post(auth, formData, create);
 router.route('/:animalId').get(getAnimal);
 router.route('/:animalId/update').put(auth, formData, update);
+router.route('/delete').delete(auth, deleteAnimal);
 
 module.exports = router;
