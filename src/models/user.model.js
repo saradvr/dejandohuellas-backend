44edff1e4860e745a686798f51a8875958ce342f
saradvr <75816184+saradvr@users.noreply.github.com>
2021-05-13 01:18:03 -0500

    creación mvc de transacciones (#7)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 const { model, models, Schema } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 const userSchema = new Schema(
   {
     name: {
@@ -10,6 +12,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: true,
+      match: [emailRegex, 'El correo no es válido'],
       validate: {
         async validator(email) {
           try {
