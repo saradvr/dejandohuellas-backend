915cac985f2712d94aaf8674bfb53b08eba4d85c
saradvr <75816184+saradvr@users.noreply.github.com>
2021-05-23 17:09:30 -0500

    User requests (#9)
    * controlador update de solicitudes
    
    * Get and delete requests
    
    * filtros de solicitudes
    
    * modificación populate en delete
    
    * format

--- src/controllers/person.controller.js
+++ src/controllers/person.controller.js
@@ -1,13 +1,34 @@
 const Person = require('../models/person.model');
 
 module.exports = {
-  async getPerson(req, res){
+  async getPerson(req, res) {
     try {
-      const { user: {userTypeId} } = req;
-      const person = await Person.findById(userTypeId).populate('user');
-      res.status(200).json({ message: 'Datos cargados con éxito.', person});
+      const {
+        user: { userTypeId },
+      } = req;
+      const person = await Person.findById(userTypeId)
+        .populate('user')
+        .populate({
+          path: 'requests',
+          select: 'animal status',
+          populate: {
+            path: 'animal',
+            select: 'name profilePicture ong',
+            populate: {
+              path: 'ong',
+              select: 'user',
+              populate: {
+                path: 'user',
+                select: 'name',
+              },
+            },
+          },
+        });
+      res.status(200).json({ message: 'Datos cargados con éxito.', person });
     } catch (error) {
-      res.status(400).json({ message: 'Error al cargar datos del usuario.', error});
+      res
+        .status(400)
+        .json({ message: 'Error al cargar datos del usuario.', error });
     }
   },
   async update(req, res) {
@@ -18,7 +39,24 @@ module.exports = {
       } = req;
       const person = await Person.findByIdAndUpdate(userTypeId, body, {
         new: true,
-      });
+      })
+        .populate('user')
+        .populate({
+          path: 'requests',
+          select: 'animal status',
+          populate: {
+            path: 'animal',
+            select: 'name profilePicture ong',
+            populate: {
+              path: 'ong',
+              select: 'user',
+              populate: {
+                path: 'user',
+                select: 'name',
+              },
+            },
+          },
+        });
       res
         .status(200)
         .json({ message: 'Datos actualizados con éxito.', person });
--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -42,4 +42,142 @@ module.exports = {
       });
     }
   },
+  async update(req, res) {
+    try {
+      const {
+        body: { status, _id },
+      } = req;
+      const request = await Request.findByIdAndUpdate(
+        _id,
+        { status: status },
+        { new: true }
+      )
+        .populate('animal')
+        .populate({
+          path: 'person',
+          select: 'phone city user -_id',
+          populate: {
+            path: 'user',
+            select: 'name email -_id',
+          },
+        });
+      res
+        .status(200)
+        .json({ message: 'Solicitud actualizada con éxito.', request });
+    } catch (error) {
+      res.status(400).json({
+        message:
+          'No se pudo guardar la solicitud, por favor intente nuevamente.',
+        error,
+      });
+    }
+  },
+  async list(req, res) {
+    try {
+      const {
+        query: { filterStatus, filterAnimalId, filterAnimalType },
+        user: { userTypeId },
+      } = req;
+      let filters = {
+        ong: userTypeId,
+      };
+      if (!!filterStatus && filterStatus !== '') {
+        filters.status = filterStatus;
+      }
+      if (!!filterAnimalId && filterAnimalId !== '') {
+        filters.animal = filterAnimalId;
+      }
+      let animals = {};
+      if (!!filterAnimalType && filterAnimalType !== '') {
+        animals = await Animal.find({ animalType: filterAnimalType });
+        filters.animal = { $in: animals };
+      }
+
+      const requests = await Request.find(filters)
+        .select('animal ong person status')
+        .populate({
+          path: 'animal',
+          select: 'name profilePicture -_id',
+        });
+      res
+        .status(200)
+        .json({ message: 'Solicitudes cargadas con éxito.', requests });
+    } catch (error) {
+      res.status(400).json({
+        message: 'Error al cargar solicitudes, intente de nuevo.',
+        error,
+      });
+    }
+  },
+  async deleteRequest(req, res) {
+    try {
+      const {
+        body: { requestId },
+        user: { userTypeId },
+      } = req;
+      let request = await Request.findById(requestId);
+      if (userTypeId.toString() === request.ong.toString()) {
+        request = await Request.findByIdAndDelete(requestId)
+          .populate('animal')
+          .populate({
+            path: 'person',
+            select: 'phone city user -_id',
+            populate: {
+              path: 'user',
+              select: 'name email -_id',
+            },
+          });
+        const ong = await Ong.findByIdAndUpdate(
+          userTypeId,
+          { $pull: { requests: requestId } },
+          { new: true }
+        );
+        const person = await Person.findByIdAndUpdate(
+          request.person,
+          { $pull: { requests: requestId } },
+          { new: true }
+        );
+        const animal = await Animal.findByIdAndUpdate(
+          request.animal,
+          { $pull: { requests: requestId } },
+          { new: true }
+        );
+        res.status(200).json({
+          message: 'Solicitud eliminada con éxito',
+          request,
+          ong,
+          person,
+          animal,
+        });
+      } else {
+        throw new Error('No tiene permiso para eliminar esta solicitud.');
+      }
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: 'Hubo un error al eliminar la solicitud.', error });
+    }
+  },
+  async getRequest(req, res) {
+    try {
+      const {
+        params: { requestId },
+      } = req;
+      const request = await Request.findById(requestId)
+        .populate('animal')
+        .populate({
+          path: 'person',
+          select: 'phone city user -_id',
+          populate: {
+            path: 'user',
+            select: 'name email -_id',
+          },
+        });
+      res
+        .status(200)
+        .json({ message: 'Solicitud cargada con éxito.', request });
+    } catch (error) {
+      res.status(400).json({ message: 'Error al cargar la solicitud.', error });
+    }
+  },
 };
